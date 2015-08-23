7ac5197ba460833ed9c766b2c5262e9ac70466ee
Georg Zoeller <georgzoeller@users.noreply.github.com>
2015-08-24 02:53:45 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,5 @@
-# Caching matrix function
+# create a matrix function capable of caching it's inverse
 # solve(A)	Inverse of A where A is a square matrix.
-
 makeCacheMatrix <- function(x = matrix()) {
   result <- NULL
   #setter
@@ -18,7 +17,7 @@ makeCacheMatrix <- function(x = matrix()) {
        getSolve = getSolve)  
 }
 
-
+# special version  of the solve (inverse) function that checks if the result is cached and returns it, or, solves and computes it
 cacheSolve <- function(x, ...) {
   # check cache
   result <- x$getSolve()
